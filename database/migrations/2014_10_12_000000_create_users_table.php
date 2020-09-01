c4554393340e3bb8db50c46d0476c8999f8f0b01
mubarak23 <mubarakaminu340@gmail.com>
2020-09-01 06:56:51 +0100

    setup three migration table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,12 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->boolean('is_admin')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softdeletes();
         });
     }
 

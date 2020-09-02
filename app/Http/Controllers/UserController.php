f82879413afaef7623a160b8545bc623fa80cd55
mubarak23 <mubarakaminu340@gmail.com>
2020-09-02 20:08:38 +0100

    user resource

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\CreateUserRequest;
+use App\Http\Requests\LoginUserRequest;
 use App\User;
 use Auth;
 
@@ -12,6 +13,35 @@ class UserController extends Controller
     //
 
     public function register_user(CreateUserRequest, $request){
-    	
+    	$data = $request->only(['name', 'email', 'password']);
+    	$data['password'] = bcrypt($data['password']);
+
+    	$user = User::create($data);
+    	$user->is_admin = 0;
+
+    	return response()->json([
+    		'user' => $user,
+    		'token' => $user->createToken('bigStore')->accessToken,
+    	])
     }
+
+    public function login(LoginUserRequest, $request){
+    	$status = 401;
+    	$response = ['error' => 'Unauthorized'];
+
+    	if(Auth::attempt($request->only(['email', 'password']))){
+    		$status = 200;
+    		$response = [
+    			'user' => Auth::user();
+    			'token' => Auth::user()->createToken('bigStore')->accessToken
+    		]
+    	}
+    	return response()->json($response, $status);
+    }
+
+
+    public function index(){
+    	return response()->json(User::with(['orders'])->get());
+    }
+
 }
